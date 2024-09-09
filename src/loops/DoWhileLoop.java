e49d0f5db83a89654c4af0e2d61247557b7030d5
Atuma David <atumadavid35@gmail.com>
2024-09-09 22:36:11 +0100

    arrays

---
+++ src/loops/DoWhileLoop.java
@@ -0,0 +1,16 @@
+package loops;
+
+import java.util.Scanner;
+
+public class DoWhileLoop {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        do {
+            System.out.println("Enter Message: ");
+             input = scanner.nextLine().toLowerCase();
+            System.out.println(input);
+        } while (!input.equals("quit"));
+    }
+}
